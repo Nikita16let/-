2ae5809865653e7c464c26275885918f2f3e9f56
Nikita16let <91055818+Nikita16let@users.noreply.github.com>
2021-10-26 10:16:10 +0300

    Add files via upload

---
+++ Лабораторная 4/листинг 4.cpp
@@ -0,0 +1,66 @@
+﻿#include<stdio.h>
+#include<locale.h>
+#include<stdlib.h>
+#include<time.h>
+void DFS(int** matrix, int st);
+int* visited = NULL;
+int a = 0;
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	int** matrix = NULL;
+	int i = 0;
+	int j = 0;
+	int sum = 0;
+	printf_s("Ââåäèòå êîëè÷åñòâî âåðøèí: ");
+	scanf_s("%d", &a);
+	srand(time(NULL));
+	matrix = (int**)malloc(a * sizeof(int));
+	visited = (int*)malloc(a * sizeof(int));
+	for (i = 0; i < a; i++) visited[i] = 0;
+	for (i = 0; i < a; i++)
+	{
+		matrix[i] = (int*)malloc(a * sizeof(int));
+	}
+	for (i = 0; i < a; i++)
+	{
+		for (j = 0; j < a; j++)
+		{
+			if (i == j)
+			{
+				matrix[i][j] = 0;
+				continue;
+			}
+
+			if (rand() % 21 > 5) matrix[i][j] = 1;
+			else matrix[i][j] = 0;
+			matrix[j][i] = matrix[i][j];
+		}
+
+	}
+
+	printf_s("Ìàòðèöà:\n");
+	for (i = 0; i < a; i++)
+	{
+		printf_s("%d|", i);
+		for (j = 0; j < a; j++)
+		{
+			printf_s("%d ", matrix[i][j]);
+		}
+		printf_s("\n");
+	}
+	printf_s("Ââåäèòå ñòàðòîâûé óçåë:");
+	int start = 0;
+	scanf_s("%d", &start);
+	DFS(matrix, start);
+}
+void DFS(int** matrix, int st)
+{
+	int i = 0;
+	visited[st] = 1;
+	printf_s("%d", st);
+	for (i = 0; i < a; i++)
+	{
+		if ((matrix[st][i] == 1) && (visited[i] == 0)) DFS(matrix, i);
+	}
+}
